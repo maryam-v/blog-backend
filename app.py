51c33537a84dd18aa6b42a11ac76665821c4d307
Maryam Valipour <maryam.valipour71@gmail.com>
2025-12-12 09:22:11 -0500

    Use DATABASE_URL for Heroku Postgres

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 # --- Database config (SQLite for now) ---
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+db_url = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
