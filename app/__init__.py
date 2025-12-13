5fa87918ce11e22b092e3e6e1f3c3409b8165d21
Maryam Valipour <maryam.valipour71@gmail.com>
2025-12-12 21:35:14 -0500

    fixed config

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask, jsonify
-from .config import DevelopmentConfig
 from .extensions import db, cors
 from .routes import register_routes
+from .config import Config
 
-def create_app(config_object=DevelopmentConfig):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_object)
+    app.config["SQLALCHEMY_DATABASE_URI"] = Config.db_uri()
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     cors.init_app(app)
     db.init_app(app)
-
     register_routes(app)
 
     @app.errorhandler(404)
--- init_db.py
+++ init_db.py
@@ -5,9 +5,10 @@ from app.models import Profile
 app = create_app()
 
 with app.app_context():
+    print("DB URI:", app.config["SQLALCHEMY_DATABASE_URI"])
+
     db.create_all()
 
-    # Seed profile row (id=1)
     profile = db.session.get(Profile, 1)
     if not profile:
         db.session.add(Profile(id=1, name="Maryam", bio="Welcome to my blog 👋"))
