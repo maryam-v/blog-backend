dbbbae72e5586e9c0854f814dcb435409c8611dd
Maryam Valipour <maryam.valipour71@gmail.com>
2025-12-12 21:37:24 -0500

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
+import os
 from flask import Flask, jsonify
 from .extensions import db, cors
 from .routes import register_routes
-from .config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = Config.db_uri()
+
+    db_url = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     cors.init_app(app)
     db.init_app(app)
+
     register_routes(app)
 
     @app.errorhandler(404)
